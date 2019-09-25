5ec1edaa1170dd77c20f3061c36a6913d8dd5b34
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-25 20:28:34 +0200

    Module 4: Working with Nullable and Non-nullable Properties

--- CS8NullBasics/Program.cs
+++ CS8NullBasics/Program.cs
@@ -3,16 +3,22 @@
 namespace CS8NullBasics
 {/*
     Understanding Non-nullable Reference Types in C# 8: 
-    Specifying That a Reference Should Be Allowed to Be Null     */
+    Working with Nullable and Non-nullable Properties     */
 
     class Program
     {
         static void Main(string[] args)
         {
-            string? message = null; /* This question mark Specifies
-            That a Reference Should Be Allowed to Be Null */
+            Message message = new Message()
+            {
+                Text = "Hello there",
+                From = null
+            };
 
-            Console.WriteLine(message);
+            Console.WriteLine(message.Text);
+            Console.WriteLine(message.From);
+            Console.WriteLine(message.ToUpperFrom());
+
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
         }
