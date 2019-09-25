f2c63e2c4bf84f41ebeb9ef79d0f6433f5845cac
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-25 19:30:44 +0200

    Module 4: Writing Some Simple Null Code

--- CS8NullBasics/Program.cs
+++ CS8NullBasics/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
 namespace CS8NullBasics
-{
+{/*
+    Understanding Non-nullable Reference Types in C# 8: 
+    Writing Some Simple Null Code
+     */
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = null;
+
+            Console.WriteLine(message);
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
         }
     }
 }
