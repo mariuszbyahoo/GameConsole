6cb21432667e6f994886f5c1890073d6322302a9
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-25 20:20:51 +0200

    Module 4: Specifying That a Reference Should Be Allowed to Be Null

--- CS8NullBasics/Program.cs
+++ CS8NullBasics/Program.cs
@@ -3,13 +3,15 @@
 namespace CS8NullBasics
 {/*
     Understanding Non-nullable Reference Types in C# 8: 
-    Enabling Non-nullable Reference Types for an Entire Project     */
+    Specifying That a Reference Should Be Allowed to Be Null     */
 
     class Program
     {
         static void Main(string[] args)
         {
-            string message = null; 
+            string? message = null; /* This question mark Specifies
+            That a Reference Should Be Allowed to Be Null */
+
             Console.WriteLine(message);
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
