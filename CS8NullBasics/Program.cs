94284a6ae35f3d33b590684a68b60fb9c8940deb
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-25 19:35:54 +0200

    Module 4: Enabling Non-nullable Reference Types for Specific Lines of Code

--- CS8NullBasics/Program.cs
+++ CS8NullBasics/Program.cs
@@ -3,15 +3,18 @@
 namespace CS8NullBasics
 {/*
     Understanding Non-nullable Reference Types in C# 8: 
-    Writing Some Simple Null Code
-     */
+    Enabling Non-nullable Reference Types for Specific Lines of Code     */
 
     class Program
     {
         static void Main(string[] args)
         {
-            string message = null;
-
+#nullable enable
+            string message = null; /* Surrounding this line with '#nullable' directives will 
+            effectively turn this 'message' variable into a non-nullable reference type, whereas before,
+            it was a nullable reference type.  In C#8 if we will set a nullable variable to a non nullable
+            one we won't get errors, instead, we get warnings. */
+#nullable disable
             Console.WriteLine(message);
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
