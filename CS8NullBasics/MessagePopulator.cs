97b9816db8579b881aa1a80fbe3b7aa7866e4ff4
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-25 20:51:40 +0200

    Module 4: Using the Null-forgiving Operator to Override the Compiler

---
+++ CS8NullBasics/MessagePopulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CS8NullBasics
+{
+    class MessagePopulator
+    {
+        public static void Populate(Message message)
+        {
+            message.GetType().InvokeMember("From",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty,
+                Type.DefaultBinder, message, new[] { "Mariusz (set using reflection)" });
+        }
+    }
+}
--- CS8NullBasics/Program.cs
+++ CS8NullBasics/Program.cs
@@ -3,7 +3,7 @@
 namespace CS8NullBasics
 {/*
     Understanding Non-nullable Reference Types in C# 8: 
-    Using the Null-coalescing and Null-conditional Operators     */
+    Using the Null-forgiving Operator to Override the Compiler     */
 
     class Program
     {
@@ -11,12 +11,20 @@ namespace CS8NullBasics
         {
             Message message = new Message()
             {
-                Text = "Hello there",
+                Text = null!, // Using the Null-Forgiving operator, in this case it effects the runtime.
                 From = null
             };
 
+            MessagePopulator.Populate(message);
+
             Console.WriteLine(message.Text);
-            Console.WriteLine(message.From ?? "No from"); // Using Null-Coalescing Operator
+            Console.WriteLine(message.From);
+            Console.WriteLine(message.From!.Length); /* Using the Null-Forgiving Operator effects
+            only the compile time, it won't automatically go and add null reference checks into the 
+            compiled program. 
+            If we will comment the Populate() line will make the program generate a NullReferenceException
+            at a runtime. */
+
             Console.WriteLine(message.ToUpperFrom());
 
             Console.WriteLine("Press Enter to exit");
